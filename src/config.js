11322029ccc4dedd4fa2a72af733b382d72e7402
Jaja Pabatao <ben@staff.zencomputes.com>
2020-01-13 13:52:29 +0800

    modified url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const constant = {
-    baseUrl: 'http://localhost:3606/',
+    // baseUrl: 'http://localhost:3606/',
+    baseUrl: 'http://ec2-3-0-183-117.ap-southeast-1.compute.amazonaws.com:3606/',
     imgUrl: 'https://bambini-gallery-thumbnails.s3-ap-southeast-1.amazonaws.com/thumbnail/',
     photoUrl: 'https://bambini-gallery-thumbnails.s3-ap-southeast-1.amazonaws.com/original/',
     originalUrl: 'https://bambini-gallery.s3-ap-southeast-1.amazonaws.com/gallery/',
